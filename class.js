e0c00096176b96286caaaf9dadb8ee35c6940c2e
lekaraidigima <you@example.com>
2021-04-24 06:41:25 -0500

    first commit

---
+++ class.js
@@ -0,0 +1,12 @@
+class Car{
+    constructor(type,year,make,model,transmission,drivetrain){
+        this.type = type;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.transmission = transmission;
+        this.drivetrain = drivetrain;
+    }
+}
+
+let car = new car(2015, "lamborghini", "Huracan", "automatic", "RWD")
